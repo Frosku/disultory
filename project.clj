d9831d0c20f83bda8834942000ba4533a9857ae7
Frosku <frosku@frosku.com>
2020-07-05 16:55:27 +0100

    Add lein project.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.frosku/disultory "0.1.0-SNAPSHOT"
+  :author "Frosku <frosku@frosku.com>"
+  :description "Library for the procedural generation of data from specification."
+  :url "https://github.com:Frosku/disultory.git"
+  :license {:name "The Unlicense"
+            :url "https://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns disultory.core}
+  :aot :all)

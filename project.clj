9fab99ab5a51f31ad84ad561e900d8a81a3df153
Frosku <frosku@frosku.com>
2020-07-06 03:27:33 +0100

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.frosku/disultory "0.1.3"
+(defproject com.frosku/disultory "0.1.4"
   :author "Frosku <frosku@frosku.com>"
   :signing {:gpg-key "frosku@frosku.com"}
   :description "Library for the procedural generation of data
